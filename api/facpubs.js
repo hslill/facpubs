2b22aa0d9414aa0ddf4d072ecd7411ec34e9c222
hslill <154374624+hslill@users.noreply.github.com>
2025-08-13 14:49:27 -0400

    Update facpubs.js

--- api/facpubs.js
+++ api/facpubs.js
@@ -1,3 +1,8 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+if (req.method === 'OPTIONS') return res.status(200).end();
+
 // /api/facpubs.js (optimized with parallel BrowZine fetches)
 import fetch from 'node-fetch';
 
